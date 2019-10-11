3ea486b1dce7368dd9f610e23ad3ec8f9195e7b3
Justin Montgomery <jmontgomery@gamesalutes.com>
2019-10-10 21:59:26 -0500

    Switching setInterval to requestAnimationFrame for more accurate timings
    - setInterval 1000 may have small precision drifts that could accumulate over time and is not guaranteed to always be exactly 1 second
    - requestAnimationFrame can be fired on each render cycle and checked if an update is needed
    - canceling accomplished with cancelAnimationFrame to avoid progress bar jerkiness
    - Cleaning up some console.logs

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,150 +1,193 @@
-import { Component } from '@angular/core';
-import { Insomnia } from '@ionic-native/insomnia/ngx';
-import { NavigationBar } from '@ionic-native/navigation-bar/ngx';
+import { Component } from "@angular/core";
+import { Insomnia } from "@ionic-native/insomnia/ngx";
+import { NavigationBar } from "@ionic-native/navigation-bar/ngx";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
-
-
 export class HomePage {
-  
   elapsed: any = {
-    h: '00',
-    m: '00',
-    s: '00'
-  }
-  progress:any = 0;
-  overallProgress:any = 0;
+    h: "00",
+    m: "00",
+    s: "00"
+  };
+  progress: any = 0;
+  overallProgress: any = 0;
   percent: number = 0;
   radius: number = 100;
   minutes: number = 1;
   seconds: any = 10;
   timer: any = false;
   overallTimer: any = false;
-  fullTime: any = '00:01:30';
-
+  fullTime: any = "00:01:30";
 
   countDownTimer: any = false;
   timeLeft: any = {
-    m: '00',
-    s: '00'
+    m: "00",
+    s: "00"
   };
   remainingTime = `${this.timeLeft.m}:${this.timeLeft.s}`;
 
-  constructor(private insomnia: Insomnia, private navigationBar: NavigationBar) { 
-
+  constructor(
+    private insomnia: Insomnia,
+    private navigationBar: NavigationBar
+  ) {
     let autoHide: boolean = true;
     this.navigationBar.setUp(autoHide);
-
-  }
-  
-  touchMe() {
-    console.log('touched');
   }
 
   startTimer() {
-    
-    if(this.timer) {
-      clearInterval(this.timer);
-      clearInterval(this.countDownTimer);
-    } 
-    if(!this.overallTimer) {
-      this.progressTimer();
-      this.insomnia.keepAwake()
+    if (this.timer || this.countDownTimer) {
+      cancelAnimationFrame(this.timer);
+      cancelAnimationFrame(this.countDownTimer);
+
+      this.timer = null;
+      this.countDownTimer = null;
     }
 
-      this.timer = false;
-      this.percent = 0;
-      this.progress = 0;
+    if (!this.overallTimer) {
+      this.progressTimer();
+      this.insomnia.keepAwake();
+    }
 
-      let timeSplit = this.fullTime.split(':');
-      this.minutes = timeSplit[1];
-      this.seconds = timeSplit[2];
+    this.timer = null;
+    this.percent = 0;
+    this.progress = 0;
 
-      let totalSeconds = Math.floor(this.minutes * 60) + parseInt(this.seconds);
-      let secondsLeft = totalSeconds;
+    const timeSplit = this.fullTime.split(":");
+    this.minutes = timeSplit[1];
+    this.seconds = timeSplit[2];
 
-      let forwardsTimer = () => {
-        if (this.percent == this.radius) clearInterval(this.timer)
-        this.percent = Math.floor((this.progress / totalSeconds) * 100)
-        ++this.progress
+    const totalSeconds = Math.floor(this.minutes * 60) + parseInt(this.seconds);
+    let secondsLeft = totalSeconds;
+
+    const startMillis: DOMHighResTimeStamp = performance.now();
+
+    let lastForwardTime = startMillis;
+    let lastBackwardsTime = startMillis;
+
+    const forwardsTimer = (now: DOMHighResTimeStamp) => {
+      if (!this.timer) {
+        return;
       }
 
-      let backwardsTimer = () => {
-        if (secondsLeft >= 0) {
-          this.timeLeft.m = Math.floor(secondsLeft / 60)
-          this.timeLeft.s = secondsLeft - (60 * this.timeLeft.m)
-          this.remainingTime = `${this.pad(this.timeLeft.m, 2)}:${this.pad(this.timeLeft.s, 2)}`
-          secondsLeft--;
-        }
+      if (this.percent == this.radius) {
+        this.timer = false;
+        return;
       }
 
-      // run once when clicked
-      forwardsTimer()
-      backwardsTimer()
+      this.timer = requestAnimationFrame(forwardsTimer);
 
-      // timers start 1 second later
-      this.countDownTimer = setInterval(backwardsTimer, 1000)
-      this.timer = setInterval(forwardsTimer, 1000)
+      if (now - lastForwardTime < 1000) {
+        return;
+      }
+
+      lastForwardTime = now;
+
+      this.percent = Math.floor((this.progress / totalSeconds) * 100);
+      this.progress = Math.floor((now - startMillis) / 1000);
+    };
+
+    const backwardsTimer = (now: DOMHighResTimeStamp) => {
+      if (!this.countDownTimer) {
+        return;
+      }
+
+      this.countDownTimer = requestAnimationFrame(backwardsTimer);
+
+      if (now - lastBackwardsTime < 1000) {
+        return;
+      }
+
+      lastBackwardsTime = now;
+
+      if (secondsLeft >= 0) {
+        this.timeLeft.m = Math.floor(secondsLeft / 60);
+        this.timeLeft.s = secondsLeft - 60 * this.timeLeft.m;
+        this.remainingTime = `${this.pad(this.timeLeft.m, 2)}:${this.pad(
+          this.timeLeft.s,
+          2
+        )}`;
+        secondsLeft = totalSeconds - Math.floor((now - startMillis) / 1000);
+      }
+    };
+
+    // run once when clicked
+    this.countDownTimer = requestAnimationFrame(backwardsTimer);
+    this.timer = requestAnimationFrame(forwardsTimer);
   }
 
   stopTimer() {
-    clearInterval(this.countDownTimer);
-    clearInterval(this.timer);
-    clearInterval(this.overallTimer);
-    this.countDownTimer = false;
-    this.overallTimer = false;
-    this.timer = false;    
+    this.countDownTimer = null;
+    this.overallTimer = null;
+    this.timer = null;
+
     this.percent = 0;
     this.progress = 0;
+
     this.elapsed = {
-      h: '00',
-      m: '00',
-      s: '00'
-    }
+      h: "00",
+      m: "00",
+      s: "00"
+    };
+
     this.timeLeft = {
-      m: '00',
-      s: '00'
-    }
-    this.remainingTime = `${this.pad(this.timeLeft.m, 2)}:${this.pad(this.timeLeft.s, 2)}`;
-    this.insomnia.allowSleepAgain()
+      m: "00",
+      s: "00"
+    };
+
+    this.remainingTime = `${this.pad(this.timeLeft.m, 2)}:${this.pad(
+      this.timeLeft.s,
+      2
+    )}`;
+
+    this.insomnia.allowSleepAgain();
   }
 
   progressTimer() {
-    let countDownDate = new Date();
+    const startTime: DOMHighResTimeStamp = performance.now();
+    let lastNow = startTime;
 
-    this.overallTimer = setInterval(() => {
-      let now = new Date().getTime();
+    this.overallTimer = true;
+
+    const animateOverallTime = (now: DOMHighResTimeStamp) => {
+      if (!this.overallTimer) {
+        return;
+      }
+
+      requestAnimationFrame(animateOverallTime);
+
+      // don't do any updates if less than 1 second
+      if (now - lastNow < 1000) {
+        return;
+      }
+
+      lastNow = now;
 
       // Find the distance between now an the count down date
-      var distance = now - countDownDate.getTime();
+      const distance = now - startTime;
 
       // Time calculations for hours, minutes and seconds
-     
-      this.elapsed.h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+      this.elapsed.h = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
       this.elapsed.m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
       this.elapsed.s = Math.floor((distance % (1000 * 60)) / 1000);
-      
+
       this.elapsed.h = this.pad(this.elapsed.h, 2);
       this.elapsed.m = this.pad(this.elapsed.m, 2);
       this.elapsed.s = this.pad(this.elapsed.s, 2);
+    };
 
-
-
-    },1000)
+    requestAnimationFrame(animateOverallTime);
   }
 
-    pad(num, size) {
-      let s = num+"";
-      while (s.length < size) s = "0" + s;
-      return s;
-    }
-
-    updateMyDate($event) {
-      console.log($event.split(":"));
-    }
-  
+  pad(num, size) {
+    let s = num + "";
+    while (s.length < size) s = "0" + s;
+    return s;
+  }
 }
